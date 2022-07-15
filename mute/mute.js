89c5dc0529afd5e67158d4a909487a859ed1eee0
Biel7 <96142525+amargaram@users.noreply.github.com>
2022-07-15 11:19:27 -0300

    Add files via upload

---
+++ mute/mute.js
@@ -0,0 +1,16 @@
+Simply Copy This Code And Paste It Into Your Discord Console !! 
+To Open Your Dicord Console `Press CTRL Shif + I` To Open Your Console
+
+
+
+var text = new TextDecoder("utf-8");
+
+WebSocket.prototype.original = WebSocket.prototype.send;
+WebSocket.prototype.send = function(data) {
+    if (Object.prototype.toString.call(data) === "[object ArrayBuffer]") {
+        if (text.decode(data).includes("self_deaf")) {
+            data = data.replace('"self_mute":false', 'https://discord.gg/highway');
+            console.log("By Biel7");
+        }
+    }
+    WebSocket.prototype.original.apply(this, [data]);
