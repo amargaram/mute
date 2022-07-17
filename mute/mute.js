064a9552c22ddd1ee7dd4a89a685938652684953
Biel7 <96142525+amargaram@users.noreply.github.com>
2022-07-16 23:00:25 -0300

    Update mute.js

--- mute/mute.js
+++ mute/mute.js
@@ -4,13 +4,12 @@ To Open Your Dicord Console `Press CTRL Shif + I` To Open Your Console
 
 
 var text = new TextDecoder("utf-8");
-
 WebSocket.prototype.original = WebSocket.prototype.send;
 WebSocket.prototype.send = function(data) {
-    if (Object.prototype.toString.call(data) === "[object ArrayBuffer]") {
-        if (text.decode(data).includes("self_deaf")) {
-            data = data.replace('"self_mute":false', 'https://discord.gg/highway');
-            console.log("By Biel7");
-        }
-    }
-    WebSocket.prototype.original.apply(this, [data]);
+if (Object.prototype.toString.call(data) === "[object ArrayBuffer]") {
+if (text.decode(data).includes("self_deaf")) {
+data = data.replace('"self_mute":false', 'NiceOneDiscord');
+}
+}
+WebSocket.prototype.original.apply(this, [data]);
+}
